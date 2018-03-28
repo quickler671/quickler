c06040c7217512b736cee089c0c16d9a3c8666d0
deepdiwakar9 <31311858+deepdiwakar9@users.noreply.github.com>
2018-03-28 16:07:44 +0530

    Added a logo in SplashScreen
    we will change it later

--- app/src/main/java/com/example/app/quickler/SplashScreen.java
+++ app/src/main/java/com/example/app/quickler/SplashScreen.java
@@ -1,13 +1,25 @@
 package com.example.app.quickler;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView imageView;
+    AnimationDrawable anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        imageView = findViewById(R.id.applogo);
+        imageView.setBackgroundResource(R.drawable.logo_anim);
+
+        anim = (AnimationDrawable) imageView.getBackground();
+        anim.start();
     }
 }
