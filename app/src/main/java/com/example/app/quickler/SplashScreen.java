258cab780c18bd220c7d86c5d988680e38a1d3e0
deepdiwakar9 <31311858+deepdiwakar9@users.noreply.github.com>
2018-03-31 10:54:09 +0530

    Added animation

--- app/src/main/java/com/example/app/quickler/SplashScreen.java
+++ app/src/main/java/com/example/app/quickler/SplashScreen.java
@@ -4,12 +4,18 @@ import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class SplashScreen extends AppCompatActivity {
 
     ImageView imageView;
     AnimationDrawable anim;
+
+    LinearLayout layout;
+    Animation myanim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +24,11 @@ public class SplashScreen extends AppCompatActivity {
 
         imageView = findViewById(R.id.applogo);
         imageView.setBackgroundResource(R.drawable.logo_anim);
-
+        layout = findViewById(R.id.linearlayout);
+        myanim = AnimationUtils.loadAnimation(this,R.anim.fadein);
         anim = (AnimationDrawable) imageView.getBackground();
+
+        layout.startAnimation(myanim);
         anim.start();
     }
 }
