6d8b3f95aaf57e9f469dcdfe3b44e69bebff424e
deepdiwakar9 <31311858+deepdiwakar9@users.noreply.github.com>
2018-03-31 23:40:20 +0530

    Added Firebase Recycler View

--- app/src/main/java/com/example/app/quickler/MainActivity.java
+++ app/src/main/java/com/example/app/quickler/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.app.quickler;
 
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     String[] typeOfOptions = {"Profile", "Explore", "Upload", "Notification", "Home", "Bookmark"};
+
+    RecyclerView recyclerView;
+    DatabaseReference mDatabase;
 
     @Override
 
@@ -52,6 +67,69 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Quickler");
+        mDatabase.keepSynced(true);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<CardData, RecyclerViewHolder> FBRA = new FirebaseRecyclerAdapter<CardData, RecyclerViewHolder>(
+                CardData.class,
+                R.layout.card_layout,
+                RecyclerViewHolder.class,
+                mDatabase)
+        {
+            @Override
+            protected void populateViewHolder(RecyclerViewHolder viewHolder, CardData model, int position) {
+                viewHolder.setTitle(model.getName());
+                
+                viewHolder.bulbbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i("TEST", "Clicked");
+                    }
+                });
+
+                viewHolder.markbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i("TEST", "Clicked");
+                    }
+                });
+
+            }
+        };
+
+        recyclerView.setAdapter(FBRA);
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder{
+
+        Button bulbbtn = itemView.findViewById(R.id.bulbbtn);
+        Button markbtn = itemView.findViewById(R.id.btnmark);
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        public void setTitle(String text)
+        {
+            TextView postTitle = itemView.findViewById(R.id.postTitle);
+            postTitle.setText(text);
+        }
     }
 }
 
+
+
+    /*viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+
+        }
+        });*/
